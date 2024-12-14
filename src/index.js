4b604fb1755f70f9fda1e2dc56e6fa01ae1ba0e1
amShuri <106229595+amShuri@users.noreply.github.com>
2024-12-14 15:43:36 -0300

    Add dropdown functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 import './style.css';
+
+function toggleDropdownVisibility(dropdownId) {
+  const visibleDropdown = document.querySelector('.visible');
+  const clickedDropdown = document.getElementById(dropdownId);
+
+  if (visibleDropdown === clickedDropdown) {
+    clickedDropdown.classList.remove('visible');
+  } else {
+    if (visibleDropdown) {
+      visibleDropdown.classList.remove('visible');
+    }
+
+    clickedDropdown.classList.add('visible');
+  }
+}
+
+function handleDropdownClick(e) {
+  const dropdownToggle = e.target.closest('.dropdown-toggle');
+  if (!dropdownToggle) return;
+
+  toggleDropdownVisibility(dropdownToggle.getAttribute('data-target'));
+}
+
+function initialize(element) {
+  document
+    .querySelector(element)
+    .addEventListener('click', handleDropdownClick);
+}
+
+initialize('nav');
