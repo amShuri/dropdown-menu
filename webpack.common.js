e0a2cca018279ec806c59671ff836643d6d1f45f
amShuri <106229595+amShuri@users.noreply.github.com>
2024-12-23 16:24:53 -0300

    Add missing webpack configs to allow bare imports

--- webpack.common.js
+++ webpack.common.js
@@ -6,6 +6,8 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    library: "Dropdown",
+    libraryTarget: "umd",
     clean: true,
   },
   plugins: [
